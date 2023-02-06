3f75c01e378f87932991c0d3a1dc66a0ccf6afd1
BeshtokovMurat <beshtokov-murat@yandex.ru>
2023-02-06 16:54:44 +0300

    Final

--- HW_3/Lesson_/Program.cs
+++ HW_3/Lesson_/Program.cs
@@ -3,18 +3,24 @@ Console.WriteLine("Введите количество кустов N > 2: ");
 int N = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите количество ягод a на кусте под номером i");
-int[] a = new int[N+1];
-for (int i = 1; i <= N; i++)
+int[] a = new int[N];
+for (int i = 0; i < a.Length; i++)
     a[i] = Convert.ToInt32(Console.ReadLine());    
-
-int max = a[1] + a[2] + a[3];
-int p = 1;
-for (int i = 1; i <= N-2; i++)
+int p = 0, p1 = 0, p2 = 0;
+int max = a[a.Length-1] + a[0] + a[1];
+p = a.Length-1; p1 = 0; p2 = 1;
+for (int i = 0; i <= a.Length-3; i++)
     {
     if (a[i] + a[i+1] + a[i+2] > max)
         {
         max = a[i] + a[i+1] + a[i+2];
-        p = i;
+        p = i; p1 = i+1; p2 = i+2;
         }        
     }
-Console.WriteLine("Максимальное количество ягод " +  max + " на кусте под номерами " + p + ", " + $"{p +1}" + ", " + $"{p +2}");
+if (a[a.Length-2] + a[a.Length-1] + a[0]>max) 
+    {
+    max = a[a.Length-2] + a[a.Length-1] + a[0];
+    p = a.Length-2; p1 = a.Length-1; p2 = 0;
+    }
+
+Console.WriteLine($"Максимальное количество ягод {max} на кусте под номерами {p+1}, {p1+1}, {p2+1}");
